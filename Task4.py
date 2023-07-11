08bd6e1b7ae70f31c1adef4e8d707dcde0a5ea8a
Sanomus <137557715+Sanomus@users.noreply.github.com>
2023-07-11 20:10:43 +0300

    update task4

--- Task4.py
+++ Task4.py
@@ -1,19 +1,22 @@
-def cashed_result(func):
-    cashe = {}
-    def wrapper(*args):
-        if args in cashe:
-            print('cashed')
-            return cashe[args]
+def cached_result(func):
+    cache = {}
+    def wrapper(*args, **kwargs):
+        if args in cache:
+            print('cached')
+            return cache[args]
+        elif kwargs:
+            print('cached')
+            return cache[tuple(kwargs.values())]
         else:
             res = func(*args)
-            cashe[args] = res
+            cache[args] = res
         return res
     return wrapper
 
 
-@cashed_result
+@cached_result
 def add(a: int, b: int) -> int:
     return a + b
 
-print(add(1,2))
-print(add(1,2))
+print(add(1, 2))
+print(add(a=1, b=2))
