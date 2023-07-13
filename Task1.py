d06561ef36bdba3dbd294cb34cb904150da43366
Sanomus <137557715+Sanomus@users.noreply.github.com>
2023-07-13 20:38:44 +0300

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,7 +1,10 @@
 def is_admin(func):
     def wrapper(**kwargs):
             try:
-                result = func(**kwargs)
+                if 'admin' not in kwargs.values():
+                    raise ValueError('Permission denied')
+                else:
+                    result = func(**kwargs)
             except ValueError as exception:
                 result = print(exception)
             return result
@@ -10,10 +13,7 @@ def is_admin(func):
 
 @is_admin
 def show_customer_receipt(user_type: str):
-    if user_type != 'admin': 
-        raise ValueError('Permission denied')
-    else:
-        print('Granny cake show_customer_receipt')
+    print('Granny cake show_customer_receipt')
 
 show_customer_receipt(user_type='user')
 show_customer_receipt(user_type='admin')
